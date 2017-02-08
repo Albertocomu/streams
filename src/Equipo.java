85c1ca58f06e551e62ef86e255caa45fc2151eb2
albertocomu <albertocomu@gmail.com>
2017-02-08 19:12:01 +0100

    Initial commit

---
+++ src/Equipo.java
@@ -0,0 +1,46 @@
+import java.time.LocalDate;
+
+public class Equipo {
+    private String nombreEquipo;
+    private String localidad;
+    private LocalDate fechaCreacion;
+
+    public Equipo(String nombreEquipo, String localidad, LocalDate fechaCreacion) {
+        this.nombreEquipo = nombreEquipo;
+        this.localidad = localidad;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" +
+                "nombreEquipo='" + nombreEquipo + '\'' +
+                ", localidad='" + localidad + '\'' +
+                ", fechaCreacion=" + fechaCreacion +
+                '}';
+    }
+}
---
+++ src/teoriaStreams/main.java
@@ -0,0 +1,89 @@
+package teoriaStreams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+public class main {
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario", "Milan");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
+    List<Transaction> transactions = Arrays.asList(
+            new Transaction(brian, 2011, 300),
+            new Transaction(raoul, 2012, 1000),
+            new Transaction(raoul, 2011, 400),
+            new Transaction(mario, 2012, 710),
+            new Transaction(mario, 2012, 700),
+            new Transaction(alan, 2012, 950)
+    );
+
+    //  Find all transactions in 2011 and sort by value (small to high)
+    List<Transaction> tr2100 =
+            transactions.stream()
+                    .filter(transaction -> transaction.getYear() == 2011)
+                    .sorted(comparing(Transaction::getValue))
+                    .collect(toList());
+
+    //  What are all the unique cities where the traders work?
+    List<String> cities =
+            transactions.stream()
+                    .map(transaction -> transaction.getTrader().getCity())
+                    .distinct()
+                    .collect(toList());
+
+    /*
+    Set<String> cities2 =
+            transactions.stream()
+                    .map(transaction -> transaction.getTrader().getCity())
+                    .collect(toSet());
+    */
+
+    //  Find all traders from Cambridge and sort them by name
+    List<Trader> traders =
+            transactions.stream()
+                    .map(Transaction::getTrader)
+                    .filter(trader -> trader.getCity().equals("Cambridge"))
+                    .distinct()
+                    .sorted(comparing(Trader::getName))
+                    .collect(toList());
+
+    //  Listing 5.4. Return a string of all traders’ names sorted alphabetically
+    String traderStr = transactions.stream()
+            .map(transaction -> transaction.getTrader().getName()).distinct()
+            .sorted()
+            .reduce("", (n1, n2) -> n1 + n2);
+
+    //  Listing 5.5. Are any traders based in Milan?
+    boolean milanBased =
+            transactions.stream()
+                    .anyMatch(transaction -> transaction.getTrader()
+                            .getCity()
+                            .equals("Milan"));
+
+    //  Listing 5.6. Print all transactions’ values from the traders living in Cambridge
+    public void living() {
+        transactions.stream()
+                .filter(t -> "Cambridge".equals(t.getTrader().getCity()))
+                .map(Transaction::getValue)
+                .forEach(System.out::println);
+    }
+
+    // 5.7. What’s the highest value of all the transactions?
+    Optional<Integer> highestValue =
+            transactions.stream()
+                    .map(Transaction::getValue)
+                    .reduce(Integer::max);
+
+    //5.8. Find the transaction with the smallest value
+    Optional<Transaction> smallestTransaction =
+            transactions.stream()
+                    .reduce((t1, t2) ->
+                            t1.getValue() < t2.getValue() ? t1 : t2);
+
+
+
+}
